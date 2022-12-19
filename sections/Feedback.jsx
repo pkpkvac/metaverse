5581f3b1ec9ddd038a22a5e97bbafe39153f326b
pkvac <pkvac>
2022-12-19 10:32:33 -0800

    fix

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -29,9 +29,8 @@ const Feedback = () => (
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          “With the development of today's technology, metaverse doesn't make
-          sense, it can be called web 3.0. By using metaverse you can lose
-          yourself”
+          “Metaverse, call it what you want, it can be called web 3.0. Be
+          careful using the metaverse, you can lose yourself”
         </p>
       </motion.div>
 
